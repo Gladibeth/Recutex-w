97307fabc770b0ef62966ca63967f9da4f3ad427
Gladibeth <gladibethdelos@gmail>
2019-09-28 16:11:02 -0400

    Add logos partners

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -63,21 +63,21 @@ $(".hamburger").on("click", function () {
 });
 
 // Font
-$(document).ready(function () {
-  WebFontConfig = {
-    google: {
-      families: ['Fira+Sans:300,400,500,600,700,800', 'Open+Sans:300,400,600,700']
-    }
-  };
-  (function () {
-    var wf = document.createElement('script');
-    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
-  })();
-});
+// $(document).ready(function () {
+//   WebFontConfig = {
+//     google: {
+//       families: ['Fira+Sans:300,400,500,600,700,800', 'Open+Sans:300,400,600,700']
+//     }
+//   };
+//   (function () {
+//     var wf = document.createElement('script');
+//     wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+//     wf.type = 'text/javascript';
+//     wf.async = 'true';
+//     var s = document.getElementsByTagName('script')[0];
+//     s.parentNode.insertBefore(wf, s);
+//   })();
+// });
 
 // $(document).ready(function () {
 //   // Add smooth scrolling to all links
